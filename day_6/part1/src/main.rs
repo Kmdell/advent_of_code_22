b56ac51913567464b77d20dc4cf261d0c54e079d
Kyle Dell <kdell1@pop-os.localdomain>
2023-04-02 21:30:27 -0400

    day 6 done

---
+++ day_6/part1/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+fn main() {
+    let file: Vec<u8> = fs::read("code.txt").unwrap();
+    
+    let index = file.windows(14).position(|x| {
+        let mut temp: u64 = 0;
+        for i in x {
+            if temp >> (*i - 65) & 1 == 1 {
+                return false;
+            } else {
+                temp = temp | (1 << *i - 65);
+            }
+        }
+        return true;
+    }).map(|x| x + 14).unwrap();
+    println!("{:?}", index);
+}
